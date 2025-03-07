05369313113185fbdf8d2675833dd8cdf3b75a10
astOwOlfo <volodimir1024@gmail.com>
2025-03-07 00:01:04 +0000

    reconfigured for dummy sf compute run

--- run_rl.sh
+++ run_rl.sh
@@ -10,15 +10,12 @@ uv run ray job submit --address="http://127.0.0.1:8265" \
   --runtime-env-json='{"setup_commands": ["pip install openrlhf[vllm]"]}' \
   --working-dir . \
   -- python -m openrlhf.cli.train_ppo_ray \
-   --ref_num_nodes 1 \
-  --ref_num_gpus_per_node 8 \
+  --ref_num_nodes 1 \
+  --ref_num_gpus_per_node 2 \
   --actor_num_nodes 1 \
-  --actor_num_gpus_per_node 8 \
-  --vllm_num_engines 2 \
-  --vllm_tensor_parallel_size 4 \
-  --colocate_all_models \
-  --vllm_gpu_memory_utilization 0.6 \
-  --vllm_enable_sleep \
+  --actor_num_gpus_per_node 4 \
+  --vllm_num_engines 1 \
+  --vllm_tensor_parallel_size 2 \
   --pretrain deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
   --save_path checkpoint/dummy_rl \
   --micro_train_batch_size 1 \
@@ -35,7 +32,7 @@ uv run ray job submit --address="http://127.0.0.1:8265" \
   --actor_learning_rate 1e-6 \
   --critic_learning_rate 1e-5 \
   --init_kl_coef 0.01 \
-  --prompt_data /home/ubuntu/vlad/bash-bench/valid_tasks-simplified-even-more-repeated.json \
+  --prompt_data /home/ubuntu/vlad/temp/swe_bench_rl/data/train.json \
   --input_key problem_statement \
   --apply_chat_template \
   --normalize_reward \
@@ -46,12 +43,16 @@ uv run ray job submit --address="http://127.0.0.1:8265" \
   --use_wandb $WANDB_API_KEY \
   --wandb_project bash_bench_rl \
   --advantage_estimator grpo \
-  --env_file bash_bench_env \
-  --env_class BashBenchEnv \
+  --env_file dummy_env \
+  --env_class DummyEnv \
   --eval_steps -1 \
-  --use_kl_loss \
-  --use_kl_estimator_k3 \
-  --enforce_eager \
+
+#   --use_kl_loss \
+#   --use_kl_estimator_k3 \
+#   --enforce_eager \
+#   --colocate_all_models \
+#   --vllm_gpu_memory_utilization 0.6 \
+#   --vllm_enable_sleep \
 
 # the following relationship should be verified:
 # micro_train_batch_size * gradient_accumulation_steps * actor num_nodes == train_batch_size
